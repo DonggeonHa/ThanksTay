2bbca64ee4bcd30f21192bd90d1d11e6ee3d7a11
reway1225 <julee9552@naver.com>
2021-08-23 18:40:31 +0900

    결제(카카오페이), 예약(카카오지도)완료

--- src/main/java/com/tt/Lodging/BookingDao.java
+++ src/main/java/com/tt/Lodging/BookingDao.java
@@ -38,5 +38,50 @@ public interface BookingDao {
 	List<AmenityListDTO> getAmenitiesByLodgingNo(int lodgingNo);
 	
 	
-	List<ReviewDTO> getReviewsByLodgingNo(int lodgingNo);
+	/**
+	 * 예약정보를 이용하여 insert한다.
+	 * @param booking 예약정보
+	 */
+	void insertBooking(BookingVO booking);
+	
+	/**
+	 * 예약이 완료되면 예약한 날짜를 price테이블에서 'Y'로 바꾼다.
+	 * @param booking
+	 */
+	void updatePrice(BookingVO booking);
+	
+	/**
+	 * 예약과 결제를 한 페이지에서 진행하다 보니까... booking을 insert하고 생성된 bookingNo가 필요하다
+	 * @param booking 방금insert한 booking정보
+	 * @return 방금 insert한 bookingNo!!!
+	 */
+	BookingVO getBookingNoByBooking(BookingVO booking);
+	
+	/**
+	 * 결제정보를 저장한다.
+	 * @param payment 결제정보
+	 */
+	void insertPayment(PaymentVO payment);
+	
+	/**
+	 * 숙소에 해당하는 리뷰 목록을 가져온다.
+	 * @param lodgingNo 숙소번호
+	 * @return 리뷰 정보
+	 */
+	List<ReviewDTO> getReviewByLodgingNo(int lodgingNo);
+	
+	/**
+	 * 유저번호와, 숙소번호에 해당하는 예약번호를 가져온다.
+	 * @param booking 유저번호와, 숙소번호를 담고있는 
+	 * @return 예약번호
+	 */
+	BookingVO getBookingNoByUserNo(BookingVO booking);
+	
+	/**
+	 * 리뷰정보를 넣어서 insert한다.
+	 * @param review 리뷰정보
+	 */
+	void insertReview(ReviewVO review);
+	
+	void updateLodging(LodgingVO lodging);
 }
